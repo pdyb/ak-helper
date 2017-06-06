03a82a4e5754cf609309d1f2ff83532a4d461b49
工口 <xinpeng.xxp@alibaba-inc.com>
2017-06-06 17:55:25 +0800

    promise diff

--- bin/akd.js
+++ bin/akd.js
@@ -10,31 +10,32 @@ const argv = require('yargs')
     .alias('n', 'nolint')
     .argv;
 
-function createDiff() {
-    log.v("\n开始ak diff...")
+function akdiff() {
+    return new Promise((resolve, reject) => {
+        log.v("\n开始ak diff...")
 
-    var ak = spawn('ak', argv.nolint ? ['diff', '--nolint', '--reviewers', '瓦雷'] : ['diff', '--reviewers', '瓦雷'], {
-        stdio: [process.stdin, process.stdout, process.stderr]
-    });
+        var ak = spawn('ak', argv.nolint ? ['diff', '--nolint', '--reviewers', '瓦雷'] : ['diff', '--reviewers', '瓦雷'], {
+            stdio: [process.stdin, process.stdout, process.stderr]
+        });
 
-    ak.on('exit', function (code) {
-        if (code == 0) {
+        ak.on('exit', function (code) {
+            if (code != 0) {
+                log.e("Unexpect!!!")
+                reject();
+                return;
+            }
+
             githelp.headCommitMsg()
                 .then((gitMsgLines) => {
-                    githelp.headCommitMsg()
-                        .then((gitMsgLines) => {
-                            var info = gitMsgLines[4];
-                            var revision = gitMsgLines[gitMsgLines.length - 3];
-                            log.v(`\n${info}\n ${revision}`);
-                        })
-                });
-        } else {
-            log.e("Unexpect!!!")
-        }
-    });
+                    var info = gitMsgLines[4];
+                    var revision = gitMsgLines[gitMsgLines.length - 3];
+                    log.v(`\n${info}\n ${revision}`);
+
+                    resolve(`${info}\n ${revision}`)
+                })
+        });
+    })
 }
 
 mk.sync()
-    .then((statusSummary) => {
-        createDiff();
-    })
+    .then(akdiff)
